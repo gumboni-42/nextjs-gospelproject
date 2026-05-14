e345e71a18654429b60f072cd7ad0d54b156b7f4
Matt Zurrer <matthias.zuerrer@gmail.com>
2026-05-14 15:57:20 +0200

    feat: add domain-based redirects to next config for external brand URLs

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,43 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  async redirects() {
+    return [
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: '(www\\.)?gospelation\\.ch',
+          },
+        ],
+        destination: 'https://gospelproject.ch/gospelation',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: '(www\\.)?gospelverein\\.ch',
+          },
+        ],
+        destination: 'https://gospelproject.ch/gospelverein',
+        permanent: true,
+      },
+      {
+        source: '/:path*',
+        has: [
+          {
+            type: 'host',
+            value: '(www\\.)?zusammenklang\\.ch',
+          },
+        ],
+        destination: 'https://gospelproject.ch/zusammenklang',
+        permanent: true,
+      },
+    ];
+  },
   images: {
     remotePatterns: [
       {
